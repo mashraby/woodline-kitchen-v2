edad693a5d3a6bf5669f09494ba6686e8304d139
Mashrab Yo'ldashov <yoldashovmashrab@gmail.com>
2023-02-19 00:26:42 +0500

    Update sidebar.tsx

--- src/components/sidebar/sidebar.tsx
+++ src/components/sidebar/sidebar.tsx
@@ -150,7 +150,7 @@ export const MiniDrawer: React.FC = () => {
   const [open, setOpen] = useState(myOpen);
   const href: string = window.location.href
     .toString()
-    .split("http://localhost:3000")[1];
+    .split("https://woodline-kitchen-version-2.vercel.app")[1];
 
   const handleDrawerOpen = () => {
     setOpen(true);
