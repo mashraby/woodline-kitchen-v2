bd0a03584fd1e1f87b4b92463c9f4968f08de34d
mashrabyuldashov <yoldashovmashrab@gmail.com>
2023-02-18 18:22:16 +0500

    fixing sidebar bug

---
+++ src/pages/orders-page/orders-page.tsx
@@ -0,0 +1,32 @@
+import React, { useContext, useEffect, useState } from "react";
+import { MiniDrawer } from "../../components/sidebar/sidebar";
+import Box from "@mui/material/Box";
+import styled from "styled-components";
+import { Typography } from "@mui/material";
+import { OrdersTable } from "./orders-table/orders-table";
+
+const FlexWrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  width: 100%;
+  margin-bottom: 12px;
+`;
+
+export const OrdersPage: React.FC = () => {
+  return (
+    <>
+      <Box sx={{ display: "flex" }}>
+        <MiniDrawer />
+        <Box component="main" sx={{ flexGrow: 1, px: 3, py: 12 }}>
+          <FlexWrapper>
+            <Typography variant="h4" component="h2">
+              Заказы
+            </Typography>
+          </FlexWrapper>
+          <OrdersTable />
+        </Box>
+      </Box>
+    </>
+  );
+};
