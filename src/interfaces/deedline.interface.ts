949eb508b848cda809f902a2bcc2da693b1862ff
mashrabyuldashov <yoldashovmashrab@gmail.com>
2023-03-09 01:48:33 +0500

    new futures

---
+++ src/interfaces/deedline.interface.ts
@@ -0,0 +1,13 @@
+export interface IDeedline {
+    _id: string
+    time: number
+}
+
+export interface IDeedlineProps {
+    deedlines: IDeedline[]
+}
+
+export interface IDeedlineModalProps {
+    open: boolean,
+    setOpen: React.Dispatch<React.SetStateAction<boolean>>
+}
--- src/pages/deedline-page/deedline-page.tsx
+++ src/pages/deedline-page/deedline-page.tsx
@@ -1,17 +1,32 @@
 import { Typography } from "@mui/material";
 import { Box } from "@mui/system";
-import React from "react";
+import React, { useEffect, useState } from "react";
 import { MiniDrawer } from "../../components/sidebar/sidebar";
+import { IDeedline } from "../../interfaces/deedline.interface";
+import { getDeedlines } from "../../services/api";
+import { DeedlineTable } from "./deedline-table/deedline-table";
 
 export const DeedlinePage: React.FC = () => {
+  const [deedlines, setDeedlines] = useState<IDeedline[]>([]);
+
+  useEffect(() => {
+    getDeedlines().then((data) => {
+      setDeedlines(data);
+    });
+  }, []);
+
   return (
-    <Box sx={{ display: "flex" }}>
-      <MiniDrawer />
-      <Box component="main" sx={{ flexGrow: 1, px: 3, py: 12 }}>
-        <Typography variant="h4" component="h2">
-          Срок действия
-        </Typography>
+    <>
+      <Box sx={{ display: "flex" }}>
+        <MiniDrawer />
+        <Box component="main" sx={{ flexGrow: 1, px: 3, py: 12 }}>
+          <Typography variant="h4" component="h2">
+            Срок действия
+          </Typography>
+
+          <DeedlineTable deedlines={deedlines} />
+        </Box>
       </Box>
-    </Box>
+    </>
   );
 };
